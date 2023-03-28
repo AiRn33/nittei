65e702e211afe4c92d2d9764eb407359416bd717
Airn33 <tkdqhr6777@naver.com>
2023-03-28 10:00:35 +0900

    3/28 초기설정(DB, 초기 설정)

---
+++ src/main/java/com/nittei/nittei/NitteiApplication.java
@@ -0,0 +1,13 @@
+package com.nittei.nittei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitteiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NitteiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nittei/nittei/NitteiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nittei.nittei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NitteiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
